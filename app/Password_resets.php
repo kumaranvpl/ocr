587da93d8d3bf1435ac17484751e232542ecedba
Kumaran Rajendhiran <kumaranvpl@gmail.com>
2016-02-18 16:51:15 +0530

    forgotpassword using tokens
    resetting passwords for forgotpassword using tokens and email
    verification finished

---
+++ app/Password_resets.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Password_resets extends Model
+{
+    //
+    protected $table = 'password_resets';
+
+    public $timestamps = false;
+}
